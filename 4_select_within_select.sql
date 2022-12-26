53bcd01319eb630d5f90e0229e70c602acfede43
Reinaldo <96353192+reipared@users.noreply.github.com>
2022-12-26 18:47:50 +0000

    Update 4_select_within_select.sql

--- 4_select_within_select.sql
+++ 4_select_within_select.sql
@@ -1,1 +1,10 @@
+-- Bigger than Russia
+-- 1. "List each country name where the population is larger than that of 'Russia'."
+-- world(name, continent, area, population, gdp)
+ SELECT name FROM world
+ WHERE population > 
+  (SELECT population FROM world
+  WHERE name = 'Russia');
 
+-- Richer Than UK
+-- 2. "Show the countries in Europe with a per capita GDP greater than 'United Kingdom'."
