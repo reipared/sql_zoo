772a41bbd730544d951456df7b479cde654b8dde
Reinaldo <96353192+reipared@users.noreply.github.com>
2022-12-27 13:29:24 +0000

    Update 7_more_join_operations.sql

--- 7_more_join_operations.sql
+++ 7_more_join_operations.sql
@@ -1,1 +1,8 @@
+-- 1962 movies
+-- 1. List the films where the "yr" is 1962 [Show "id", "title"]
+SELECT id, title 
+FROM movie
+WHERE yr = 1962
 
+-- When was Citizen Kane released?
+-- 2. Give year of 'Citizen Kane'.
